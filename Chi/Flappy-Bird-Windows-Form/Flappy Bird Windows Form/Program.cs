b034a86d09db15b34c273d47fc2d89cbe620c9e9
hoang795 <ggwp240105@gmail.com>
2024-12-16 21:44:05 +0700

    Add files via upload

---
+++ Chi/Flappy-Bird-Windows-Form/Flappy Bird Windows Form/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird_Windows_Form
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Mở màn hình MainMenu đầu tiên
+            Application.Run(new MainMenu());
+        }
+    }
+}
+}
